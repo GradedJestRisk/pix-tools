4edb81a04355f4ab988505532f64e4234776a9c5
GradedJestRisk <graded.jest.risk@gmail.com>
2023-09-11 17:43:17 +0200

    Add SSH generation

--- install.sh
+++ install.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
 
+# Bootstrap folders
 mkdir Pix && cd Pix
 
 # Clone main repositories
 mkdir repositories && cd repositories
-git clone git@github.com:1024pix/pix.git && cd ..
-git clone git@github.com:1024pix/pix-bot.git && cd ..
-git clone git@github.com:1024pix/pix-db-replication.git && cd ..
-git clone git@github.com:1024pix/pix-db-stats.git && cd ..
-git clone git@github.com:1024pix/pix-dev-tools.git && cd ..
-git clone git@github.com:1024pix/pix-dns.git && cd ..
-git clone git@github.com:1024pix/pix-editor.git && cd ..
-git clone git@github.com:1024pix/pix-engineering.git && cd ..
-git clone git@github.com:1024pix/pix-infrastructure.git && cd ..
-git clone git@github.com:1024pix/pix-lifestyle.git && cd ..
-git clone git@github.com:1024pix/pix-review-router.git && cd ..
-git clone git@github.com:1024pix/steampipe-checks.git && cd ..
+git clone git@github.com:1024pix/pix.git
+git clone git@github.com:1024pix/pix-bot.git
+git clone git@github.com:1024pix/pix-db-replication.git
+git clone git@github.com:1024pix/pix-db-stats.git
+git clone git@github.com:1024pix/pix-dev-tools.git
+git clone git@github.com:1024pix/pix-dns.git
+git clone git@github.com:1024pix/pix-editor.git
+git clone git@github.com:1024pix/pix-engineering.git
+git clone git@github.com:1024pix/pix-infrastructure.git
+git clone git@github.com:1024pix/pix-lifestyle.git
+git clone git@github.com:1024pix/pix-review-router.git
+git clone git@github.com:1024pix/steampipe-checks.git
