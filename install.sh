e0478debe0d5454fe75844e4997fe279bac91d92
GradedJestRisk <graded.jest.risk@gmail.com>
2023-09-13 19:37:48 +0200

    Back me up

--- install.sh
+++ install.sh
@@ -6,9 +6,9 @@ mkdir Pix && cd Pix
 # Clone main repositories
 mkdir repositories && cd repositories
 git clone git@github.com:1024pix/pix.git
-git clone git@github.com:1024pix/pix-bot.git
-git clone git@github.com:1024pix/pix-db-replication.git
-git clone git@github.com:1024pix/pix-db-stats.git
+git clone git@github.com:1024pix/pix-bot.git && cd pix-bot && npm ci && cd ..
+git clone git@github.com:1024pix/pix-db-replication.git && cd pix-db-replication && npm ci && cd ..
+git clone git@github.com:1024pix/pix-db-stats.git && cd pix-db-stats && npm ci && cd ..
 git clone git@github.com:1024pix/pix-dev-tools.git
 git clone git@github.com:1024pix/pix-dns.git
 git clone git@github.com:1024pix/pix-editor.git
